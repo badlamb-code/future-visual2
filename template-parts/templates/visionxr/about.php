294eb69c658664a22fe4cf19f96965ae4639c3a4
Baillie O'Grady <hello@baillieogrady.com>
2019-11-03 13:55:43 +0000

    Fix triangle backgrounds

--- template-parts/templates/visionxr/about.php
+++ template-parts/templates/visionxr/about.php
@@ -1,20 +1,21 @@
 <!-- visionxr-about -->
 <section class="visionxr-about" id="visionxr-about">
-	<div class="visionxr-about__bg bg-cover"></div>
-	<div class="container">
-		<div class="visionxr-about__container">
-			<h2 class="color-white text-center ma0">About VISIONxR<sup>TM<sup></h2>
-			<div class="visionxr-about__box display-flex align-center">
-				<img src="<?php echo get_template_directory_uri() . '/assets/images/about.svg'; ?>" alt="About">
-				<p class="color-white">
-					A paragraph summarising VISIONxR’s absolute top-level proposition, about three lines in length. Lorem ipsum dolor sit amet consectetur adipiscing elit. Morbi sagittis magna vel libero commodo, quis sollicitudin nisi interdum. 
-				</p>
-			</div>
-			<div class="visionxr-about__box display-flex align-center justify-between">
-				<p class="color-white bold">
-					Collaborate, communicate, solve problems, train, and learn together.
-				</p>
-				<img src="<?php echo get_template_directory_uri() . '/assets/images/collaboration.svg'; ?>" alt="Collaboration">
+	<h2 class="color-white text-center ma0">About VISIONxR<sup>TM<sup></h2>
+	<div class="visionxr-about__bg">
+		<div class="container">
+			<div class="visionxr-about__container">
+				<div class="visionxr-about__box display-flex align-center">
+					<img src="<?php echo get_template_directory_uri() . '/assets/images/about.svg'; ?>" alt="About">
+					<p class="color-white">
+						A paragraph summarising VISIONxR’s absolute top-level proposition, about three lines in length. Lorem ipsum dolor sit amet consectetur adipiscing elit. Morbi sagittis magna vel libero commodo, quis sollicitudin nisi interdum. 
+					</p>
+				</div>
+				<div class="visionxr-about__box display-flex align-center justify-between">
+					<p class="color-white bold">
+						Collaborate, communicate, solve problems, train, and learn together.
+					</p>
+					<img src="<?php echo get_template_directory_uri() . '/assets/images/collaboration.svg'; ?>" alt="Collaboration">
+				</div>
 			</div>
 		</div>
 	</div>
--- template-parts/templates/visionxr/why.php
+++ template-parts/templates/visionxr/why.php
@@ -1,43 +1,44 @@
 <!-- visionxr-why -->
 <section class="visionxr-why" id="visionxr-why">
-	<div class="visionxr-why__bg bg-cover bg-cover"></div>
-	<div class="container">
-		<h2 class="ma0 color-white text-center">Why VISIONxR?</h2>
-		<div class="visionxr-why__inner">
-			<div class="visionxr-why__box">
-				<div>
-					<p>94%</p>
+	<h2 class="ma0 color-white text-center">Why VISIONxR?</h2>
+	<div class="visionxr-why__bg">
+		<div class="container">
+			<div class="visionxr-why__inner">
+				<div class="visionxr-why__box">
+					<div>
+						<p>94%</p>
+					</div>
+					<div>
+						<p>of trainees want <span>more VR</span></p>
+					</div>
 				</div>
-				<div>
-					<p>of trainees want <span>more VR</span></p>
+				<div class="visionxr-why__box">
+					<div>
+						<p>75%</p>
+					</div>
+					<div>
+						<p>retention rate with VR</p>
+					</div>
 				</div>
-			</div>
-			<div class="visionxr-why__box">
-				<div>
-					<p>75%</p>
+				<div class="visionxr-why__box">
+					<div>
+						<p>80%</p>
+					</div>
+					<div>
+						<p>
+							of employees believe they are better prepared for their job after VR training
+						</p>
+					</div>
 				</div>
-				<div>
-					<p>retention rate with VR</p>
-				</div>
-			</div>
-			<div class="visionxr-why__box">
-				<div>
-					<p>80%</p>
-				</div>
-				<div>
-					<p>
-						of employees believe they are better prepared for their job after VR training
-					</p>
-				</div>
-			</div>
-			<div class="visionxr-why__box">
-				<div>
-					<p>82%</p>
-				</div>
-				<div>
-					<p>
-						of trainees say VR is better than any other training
-					</p>
+				<div class="visionxr-why__box">
+					<div>
+						<p>82%</p>
+					</div>
+					<div>
+						<p>
+							of trainees say VR is better than any other training
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
